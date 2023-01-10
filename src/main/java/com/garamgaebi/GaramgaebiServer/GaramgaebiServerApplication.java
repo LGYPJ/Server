77b4697dc594130dfedac655dea3bea46091d043
Jodongjin <ehdwlsdlwkd@naver.com>
2023-01-11 00:40:18 +0900

    Spring Boot Template setting

---
+++ src/main/java/com/garamgaebi/GaramgaebiServer/GaramgaebiServerApplication.java
@@ -0,0 +1,13 @@
+package com.garamgaebi.GaramgaebiServer;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GaramgaebiServerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GaramgaebiServerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/garamgaebi/GaramgaebiServer/GaramgaebiServerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.garamgaebi.GaramgaebiServer;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GaramgaebiServerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
