62ad9289dea94ea40d87b4c745a7244ef2eb0a57
seock1000 <tjrcjs0705@gmail.com>
2023-01-27 17:55:17 +0900

    Refactor: Swagger 에러코드 추가

--- src/main/java/com/garamgaebi/GaramgaebiServer/domain/program/controller/NetworkingController.java
+++ src/main/java/com/garamgaebi/GaramgaebiServer/domain/program/controller/NetworkingController.java
@@ -4,6 +4,9 @@ import com.garamgaebi.GaramgaebiServer.domain.program.dto.*;
 import com.garamgaebi.GaramgaebiServer.domain.program.service.NetworkingService;
 import com.garamgaebi.GaramgaebiServer.global.response.BaseResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Content;
+import io.swagger.v3.oas.annotations.media.Schema;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
@@ -25,13 +28,16 @@ public class NetworkingController {
 
     @Operation(summary = "이번 달 네트워킹 조회", description = "이번 달에 있는 네트워킹을 한 건을 조회합니다.")
     @ApiResponses( value = {
-            @ApiResponse(responseCode = "200", description = "성공")
+            @ApiResponse(responseCode = "200", description = "성공"),
+            @ApiResponse(responseCode = "500", description = "알 수 없는 서버에러", content = @Content())
     })
+    @Parameter()
     @GetMapping("/this-month")
     public BaseResponse<ProgramDto> getThisMonthNetworking() { return new BaseResponse<>(networkingService.findThisMonthNetworking()); }
 
     @Operation(summary = "예정된 네트워킹 조회", description = "다음 달 이후 가장 최근에 있을 네트워킹을 한 건을 조회합니다.", responses = {
-            @ApiResponse(responseCode = "200", description = "성공")
+            @ApiResponse(responseCode = "200", description = "성공"),
+            @ApiResponse(responseCode = "500", description = "알 수 없는 서버에러", content = @Content())
     })
     // 예정된 네트워킹
     @GetMapping("/next-month")
@@ -40,7 +46,8 @@ public class NetworkingController {
     }
 
     @Operation(summary = "마감된 네트워킹 리스트 조회", description = "마감된 네트워킹들을 리스트로 조회합니다.", responses = {
-            @ApiResponse(responseCode = "200", description = "성공")
+            @ApiResponse(responseCode = "200", description = "성공"),
+            @ApiResponse(responseCode = "500", description = "알 수 없는 서버에러", content = @Content())
     })
     // 마감된 네트워킹
     @GetMapping("/closed")
@@ -49,7 +56,8 @@ public class NetworkingController {
     }
 
     @Operation(summary = "홈 화면 네트워킹 조회", description = "모든 네트워킹 리스트를 이번 달/예정/마감 순서로 조회합니다.", responses = {
-            @ApiResponse(responseCode = "200", description = "성공")
+            @ApiResponse(responseCode = "200", description = "성공"),
+            @ApiResponse(responseCode = "500", description = "알 수 없는 서버에러", content = @Content())
     })
     //홈 화면 네트워킹 리스트
     @GetMapping("/main")
@@ -59,7 +67,10 @@ public class NetworkingController {
 
     // 네트워킹 상세 정보
     @Operation(summary = "네트워킹 상세정보 조회", description = "네트워킹 상세페이지 상단 상세 정보를 조회합니다.", responses = {
-            @ApiResponse(responseCode = "200", description = "성공")
+            @ApiResponse(responseCode = "200", description = "성공"),
+            @ApiResponse(responseCode = "2001", description = "존재하지 않는 회원", content = @Content()),
+            @ApiResponse(responseCode = "2002", description = "존재하지 않는 프로그램", content = @Content()),
+            @ApiResponse(responseCode = "500", description = "알 수 없는 서버에러", content = @Content())
     })
     @GetMapping("/info")
     public BaseResponse<ProgramInfoDto> getNetworkingDetailInfo(@RequestBody @Valid ProgramDetailReq programDetailReq) {
@@ -69,7 +80,9 @@ public class NetworkingController {
 
     // 네트워킹 신청자 리스트
     @Operation(summary = "네트워킹 신청자 리스트 조회", description = "네트워킹 신청자를 리스트로 조회합니다.", responses = {
-            @ApiResponse(responseCode = "200", description = "성공")
+            @ApiResponse(responseCode = "200", description = "성공"),
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 리소스", content = @Content()),
+            @ApiResponse(responseCode = "500", description = "알 수 없는 서버에러", content = @Content())
     })
     @GetMapping("/{networking-idx}/participants")
     public BaseResponse<List<ParticipantDto>> getNetworkingParticipantList(@PathVariable(name = "networking-idx") Long networkingIdx) {
--- src/main/java/com/garamgaebi/GaramgaebiServer/domain/program/controller/SeminarController.java
+++ src/main/java/com/garamgaebi/GaramgaebiServer/domain/program/controller/SeminarController.java
@@ -4,12 +4,10 @@ import com.garamgaebi.GaramgaebiServer.domain.program.dto.*;
 import com.garamgaebi.GaramgaebiServer.domain.program.service.SeminarService;
 import com.garamgaebi.GaramgaebiServer.global.response.BaseResponse;
 import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
-import jakarta.validation.constraints.NotEmpty;
-import jakarta.validation.constraints.NotNull;
-import jakarta.validation.constraints.PositiveOrZero;
 import lombok.RequiredArgsConstructor;
 import org.springframework.web.bind.annotation.*;
 
@@ -25,7 +23,8 @@ public class SeminarController {
 
     // 이번 달 세미나
     @Operation(summary = "이번 달 세미나 조회", description = "이번 달에 있는 세미나 한 건을 조회합니다.", responses = {
-            @ApiResponse(responseCode = "200", description = "성공")
+            @ApiResponse(responseCode = "200", description = "성공"),
+            @ApiResponse(responseCode = "500", description = "알 수 없는 서버 에러", content = @Content())
     })
     @GetMapping("/this-month")
     public BaseResponse<ProgramDto> getThisMonthSeminar() {
@@ -34,7 +33,8 @@ public class SeminarController {
 
     // 예정된 세미나
     @Operation(summary = "예정된 세미나 조회", description = "다음 달 이후 가장 최근에 있을 세미나 한 건을 조회합니다.", responses = {
-            @ApiResponse(responseCode = "200", description = "성공")
+            @ApiResponse(responseCode = "200", description = "성공"),
+            @ApiResponse(responseCode = "500", description = "알 수 없는 서버 에러", content = @Content())
     })
     @GetMapping("/next-month")
     public BaseResponse<ProgramDto> getNextSeminar() {
@@ -43,7 +43,8 @@ public class SeminarController {
 
     // 마감된 세미나
     @Operation(summary = "마감된 세미나 조회", description = "마감된 세미나를 리스트로 조회합니다.", responses = {
-            @ApiResponse(responseCode = "200", description = "성공")
+            @ApiResponse(responseCode = "200", description = "성공"),
+            @ApiResponse(responseCode = "500", description = "알 수 없는 서버 에러", content = @Content())
     })
     @GetMapping("/closed")
     public BaseResponse<List<ProgramDto>> getClosedSeminarList() {
@@ -52,14 +53,18 @@ public class SeminarController {
 
     //홈 화면 세미나 리스트
     @Operation(summary = "홈 화면 세미나 조회", description = "모든 세미나 리스트를 이번 달/예정/마감 순서로 조회합니다.", responses = {
-            @ApiResponse(responseCode = "200", description = "성공")
+            @ApiResponse(responseCode = "200", description = "성공"),
+            @ApiResponse(responseCode = "500", description = "알 수 없는 서버 에러", content = @Content())
     })
     @GetMapping("/main")
     public BaseResponse<List<ProgramDto>> getMainSeminarList() { return new BaseResponse<>(seminarService.findMainSeminarList()); }
 
     // 세미나 상세정보
     @Operation(summary = "세미나 상세정보 조회", description = "세미나 상세페이지 상단 부분인 세미나 상세정보를 조회합니다.", responses = {
-            @ApiResponse(responseCode = "200", description = "성공")
+            @ApiResponse(responseCode = "200", description = "성공"),
+            @ApiResponse(responseCode = "2001", description = "존재하지 않는 회원입니다.", content = @Content()),
+            @ApiResponse(responseCode = "2002", description = "존재하지 않는 프로그램입니다.", content = @Content()),
+            @ApiResponse(responseCode = "500", description = "알 수 없는 서버 에러", content = @Content())
     })
     @GetMapping("/info")
     public BaseResponse<ProgramInfoDto> getSeminarDetailInfo(@RequestBody @Valid ProgramDetailReq programDetailReq) {
@@ -69,7 +74,9 @@ public class SeminarController {
 
     // 세미나 신청자 리스트
     @Operation(summary = "세미나 신청자 리스트 조회", description = "세미나의 신청자를 리스트로 조회합니다.", responses = {
-            @ApiResponse(responseCode = "200", description = "성공")
+            @ApiResponse(responseCode = "200", description = "성공"),
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 리소스", content = @Content()),
+            @ApiResponse(responseCode = "500", description = "알 수 없는 서버 에러", content = @Content())
     })
     @GetMapping("/{seminar-idx}/participants")
     public BaseResponse<List<ParticipantDto>> getSeminarParticipantList(@PathVariable(name = "seminar-idx") Long seminarIdx) {
@@ -79,7 +86,9 @@ public class SeminarController {
 
     // 세미나 발표 리스트
     @Operation(summary = "세미나 발표 리스트 조회", description = "세미나의 발표자료를 리스트로 조회합니다.", responses = {
-            @ApiResponse(responseCode = "200", description = "성공")
+            @ApiResponse(responseCode = "200", description = "성공"),
+            @ApiResponse(responseCode = "404", description = "존재하지 않는 리소스", content = @Content()),
+            @ApiResponse(responseCode = "500", description = "알 수 없는 서버 에러", content = @Content())
     })
     @GetMapping("/{seminar-idx}/presentations")
     public BaseResponse<List<PresentationDto>> getSeminarPresentationList(@PathVariable(name = "seminar-idx") Long seminarIdx) {
