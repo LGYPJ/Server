d96b84db068ed7568acada3787a5ce53dcc8d09e
seock1000 <tjrcjs0705@gmail.com>
2023-02-02 20:21:24 +0900

    Fix: notification config 에러 해결

--- src/main/java/com/garamgaebi/GaramgaebiServer/domain/notification/listener/ApplyEventListenerImpl.java
+++ src/main/java/com/garamgaebi/GaramgaebiServer/domain/notification/listener/ApplyEventListenerImpl.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Component
-@Async("applyThreadPoolExecutor")
+//@Async("applyThreadPoolExecutor")
 @Transactional
 @RequiredArgsConstructor
 public class ApplyEventListenerImpl implements ApplyEventListener {
