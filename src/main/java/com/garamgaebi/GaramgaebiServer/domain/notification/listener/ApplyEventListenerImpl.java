38c91a6afb15d5f0410f1ad3dd21eb80d71ae911
seock1000 <tjrcjs0705@gmail.com>
2023-02-02 19:52:15 +0900

    Feat: 빈 생성 충돌 에러 해결

--- src/main/java/com/garamgaebi/GaramgaebiServer/domain/notification/listener/ApplyEventListenerImpl.java
+++ src/main/java/com/garamgaebi/GaramgaebiServer/domain/notification/listener/ApplyEventListenerImpl.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Component
-@Async("apply_async")
+@Async("applyThreadPoolExecutor")
 @Transactional
 @RequiredArgsConstructor
 public class ApplyEventListenerImpl implements ApplyEventListener {
--- src/main/java/com/garamgaebi/GaramgaebiServer/domain/notification/listener/ProgramEventListenerImpl.java
+++ src/main/java/com/garamgaebi/GaramgaebiServer/domain/notification/listener/ProgramEventListenerImpl.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Component
-@Async("program_async")
+@Async("programThreadPoolExecutor")
 @Transactional
 @RequiredArgsConstructor
 public class ProgramEventListenerImpl implements ProgramEventListener {
