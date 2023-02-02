6dcec93be4748b45d03d1d5dcb9f364ed7474a6c
seock1000 <tjrcjs0705@gmail.com>
2023-02-02 19:43:06 +0900

    Fix: 빈 생성 충돌 에러 해결

--- src/main/java/com/garamgaebi/GaramgaebiServer/domain/notification/listener/ApplyEventListenerImpl.java
+++ src/main/java/com/garamgaebi/GaramgaebiServer/domain/notification/listener/ApplyEventListenerImpl.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Component
-@Async("apply")
+@Async("apply_async")
 @Transactional
 @RequiredArgsConstructor
 public class ApplyEventListenerImpl implements ApplyEventListener {
--- src/main/java/com/garamgaebi/GaramgaebiServer/domain/notification/listener/ProgramEventListenerImpl.java
+++ src/main/java/com/garamgaebi/GaramgaebiServer/domain/notification/listener/ProgramEventListenerImpl.java
@@ -14,7 +14,7 @@ import java.util.ArrayList;
 import java.util.List;
 
 @Component
-@Async("program")
+@Async("program_async")
 @Transactional
 @RequiredArgsConstructor
 public class ProgramEventListenerImpl implements ProgramEventListener {
