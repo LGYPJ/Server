18640a05a534c36a13f834ea52da25a010afbd6c
rhatnsdj <rhatnsdj@naver.com>
2023-02-07 00:19:51 +0900

    Refactor: 관리자 페이지 프로그램 생성,수정,삭제 Validation 수정

--- src/main/java/com/garamgaebi/GaramgaebiServer/admin/program/dto/NetworkingDto.java
+++ src/main/java/com/garamgaebi/GaramgaebiServer/admin/program/dto/NetworkingDto.java
@@ -5,6 +5,7 @@ import com.garamgaebi.GaramgaebiServer.domain.entity.ProgramStatus;
 import com.garamgaebi.GaramgaebiServer.domain.entity.ProgramType;
 import jakarta.validation.constraints.Future;
 import jakarta.validation.constraints.NotBlank;
+import jakarta.validation.constraints.NotNull;
 import lombok.*;
 import org.springframework.format.annotation.DateTimeFormat;
 
@@ -17,7 +18,7 @@ import java.time.LocalDateTime;
 public class NetworkingDto {
     @NotBlank(message = "네트워킹명을 입력해주세요.")
     private String title;
-    @NotBlank(message = "네트워킹 날짜를 입력해주세요.")
+    @NotNull
     @Future
     private LocalDateTime date;
     @NotBlank(message = "네트워킹 장소를 입력해주세요.")
