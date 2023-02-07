a0746058cae9f00623e7ae1462d60144e2180d3e
seock1000 <tjrcjs0705@gmail.com>
2023-02-07 23:00:07 +0900

    Chore: S3 에러 확인

--- src/main/java/com/garamgaebi/GaramgaebiServer/global/config/security/SecurityConfig.java
+++ src/main/java/com/garamgaebi/GaramgaebiServer/global/config/security/SecurityConfig.java
@@ -37,6 +37,7 @@ public class SecurityConfig {
                 .and()
                 .authorizeHttpRequests()
                 .requestMatchers(PERMIT_URL_ARRAY).permitAll()
+                .requestMatchers("/profile/**").permitAll()
                 .requestMatchers("/images").permitAll()
                 .requestMatchers("/admin/**").permitAll()
                 .requestMatchers("/member/login").permitAll()
