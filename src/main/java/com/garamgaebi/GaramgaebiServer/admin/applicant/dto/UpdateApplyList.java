709813ee0a98bcf1980635b0e9e69f156ce9496d
Cjunhyun <48709581+Cjunhyun@users.noreply.github.com>
2023-02-13 21:32:14 +1000

    Refactor: Admin 프로그램 신청자 상태 수정API
    Refactor: Admin 프로그램 신청자 상태 수정API
    
    -체크표시에 맞게 상태 수정

--- src/main/java/com/garamgaebi/GaramgaebiServer/admin/applicant/dto/UpdateApplyList.java
+++ src/main/java/com/garamgaebi/GaramgaebiServer/admin/applicant/dto/UpdateApplyList.java
@@ -1,22 +1,13 @@
 package com.garamgaebi.GaramgaebiServer.admin.applicant.dto;
 
-
 import lombok.*;
-
-import java.time.LocalDateTime;
-import java.util.Date;
 
 @Getter
 @Setter
 @AllArgsConstructor
 @NoArgsConstructor(access = AccessLevel.PUBLIC)
-public class ApplyList {
+public class UpdateApplyList {
     private Long memberIdx;
     private String name;
-    private String nickName;
-    private String phone;
-    private String bank;
-    private String account;
     private Boolean status;
-    private LocalDateTime updatedAt;
 }
