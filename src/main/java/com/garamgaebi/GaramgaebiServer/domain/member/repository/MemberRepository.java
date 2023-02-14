3ca8dea2a5cb5af6d4f9aea43c23c433a83dc02a
Jodongjin <ehdwlsdlwkd@naver.com>
2023-02-15 00:32:24 +0900

    Chore: password 없애기 전 commit

--- src/main/java/com/garamgaebi/GaramgaebiServer/domain/member/repository/MemberRepository.java
+++ src/main/java/com/garamgaebi/GaramgaebiServer/domain/member/repository/MemberRepository.java
@@ -3,6 +3,7 @@ package com.garamgaebi.GaramgaebiServer.domain.member.repository;
 import com.garamgaebi.GaramgaebiServer.domain.entity.Member;
 import com.garamgaebi.GaramgaebiServer.domain.entity.status.member.MemberStatus;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
 
 import java.util.List;
 import java.util.Optional;
@@ -11,4 +12,7 @@ public interface MemberRepository extends JpaRepository<Member, Long> {
     Optional<Member> findByUniEmail(String uniEmail);
 
     List<Member> findByStatus(MemberStatus memberStatus);
+
+    @Query("select memberIdx from Member order by memberIdx desc limit 1")
+    Member findLastIdx();
 }
