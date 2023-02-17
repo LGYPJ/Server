c7a5a2f5205869eb2c139e03d034529b76f8723d
seock1000 <tjrcjs0705@gmail.com>
2023-02-18 01:26:52 +0900

    Refactor: notification domain 리팩터링

--- src/main/java/com/garamgaebi/GaramgaebiServer/domain/notification/service/NotificationService.java
+++ src/main/java/com/garamgaebi/GaramgaebiServer/domain/notification/service/NotificationService.java
@@ -1,10 +1,13 @@
 package com.garamgaebi.GaramgaebiServer.domain.notification.service;
 
+import com.garamgaebi.GaramgaebiServer.domain.entity.Member;
+import com.garamgaebi.GaramgaebiServer.domain.entity.Notification;
 import com.garamgaebi.GaramgaebiServer.domain.notification.dto.GetNotificationDto;
 import com.garamgaebi.GaramgaebiServer.domain.notification.dto.GetNotificationResDto;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.domain.Slice;
+import org.springframework.transaction.annotation.Transactional;
 
 import java.util.List;
 
@@ -12,4 +15,8 @@ public interface NotificationService {
     public GetNotificationResDto getMemberNotificationList(Long memberIdx, Long lastNotificationIdx);
 
     public Boolean isMemberNotificationExist(Long memberIdx);
+
+    public void addNotification(Notification notification, List<Member> members);
+
+    public void addNotification(Notification notification, Member member);
 }
