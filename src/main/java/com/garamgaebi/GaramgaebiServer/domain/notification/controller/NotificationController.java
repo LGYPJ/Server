ef393863a61f994c5e16f0384b0c85415974c538
seock1000 <tjrcjs0705@gmail.com>
2023-02-20 20:43:52 +0900

    Refactor: notification domain 리팩터링

--- src/main/java/com/garamgaebi/GaramgaebiServer/domain/notification/controller/NotificationController.java
+++ src/main/java/com/garamgaebi/GaramgaebiServer/domain/notification/controller/NotificationController.java
@@ -1,7 +1,6 @@
 package com.garamgaebi.GaramgaebiServer.domain.notification.controller;
 
-import com.garamgaebi.GaramgaebiServer.domain.notification.dto.GetNotificationDto;
-import com.garamgaebi.GaramgaebiServer.domain.notification.dto.GetNotificationResDto;
+import com.garamgaebi.GaramgaebiServer.domain.notification.dto.response.GetNotificationResDto;
 import com.garamgaebi.GaramgaebiServer.domain.notification.service.NotificationService;
 import com.garamgaebi.GaramgaebiServer.global.response.BaseResponse;
 import io.swagger.v3.oas.annotations.Operation;
@@ -9,22 +8,11 @@ import io.swagger.v3.oas.annotations.media.Content;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
-import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
-import org.springframework.data.domain.Page;
-import org.springframework.data.domain.Pageable;
-import org.springframework.data.domain.Slice;
-import org.springframework.data.domain.Sort;
-import org.springframework.data.web.PageableDefault;
-import org.springframework.data.web.SortDefault;
 import org.springframework.web.bind.annotation.*;
 
-import java.util.Collection;
-import java.util.List;
 import java.util.Map;
-import java.util.Set;
 import java.util.concurrent.ConcurrentHashMap;
-import java.util.concurrent.ConcurrentMap;
 
 @Tag(name = "NotificationController", description = "알림 컨트롤러(담당자:로니)")
 @RestController
@@ -34,6 +22,10 @@ public class NotificationController {
 
     private final NotificationService notificationService;
 
+    /**
+     * 유저의 알림 리스트 조회
+     * @Author 로니
+     */
     @Operation(summary = "유저 알림 화면 조회", description = "유저의 알림 목록을 10개씩 페이징하여 조회합니다.")
     @ApiResponses( value = {
             @ApiResponse(responseCode = "200", description = "성공"),
@@ -47,6 +39,10 @@ public class NotificationController {
         return new BaseResponse<>(notificationService.getMemberNotificationList(memberIdx, lastNotificationIdx));
     }
 
+    /**
+     * 읽지 않은 알림 존재 여부 확인
+     * @Author 로니
+     */
     @Operation(summary = "유저의 읽지 않은 알림 존재 여부", description = "유저가 읽지 않은 알림이 존재한다면 TRUE, 그렇지 않으면 FALSE를 반환합니다.")
     @ApiResponses( value = {
             @ApiResponse(responseCode = "200", description = "성공"),
