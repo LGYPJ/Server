4987a41bfcb1fa0b1980e8a461e6ef6d9937cc42
seock1000 <tjrcjs0705@gmail.com>
2023-02-20 19:07:06 +0900

    Fix: 이미 있는 토큰 저장 방지

--- src/main/java/com/garamgaebi/GaramgaebiServer/domain/member/service/MemberServiceImpl.java
+++ src/main/java/com/garamgaebi/GaramgaebiServer/domain/member/service/MemberServiceImpl.java
@@ -132,7 +132,8 @@ public class MemberServiceImpl implements MemberService {
                 tokenInfo.getRefreshToken(),
                 tokenInfo.getRefreshTokenExpirationTime());
 
-        if(!memberLoginReq.getFcmToken().isBlank()) {
+        if(!memberLoginReq.getFcmToken().isBlank()
+                && member.getMemberFcms().stream().noneMatch(memberFcm -> memberFcm.getFcmToken().equals(memberLoginReq.getFcmToken()))) {
             member.addMemberFcms(MemberFcm.builder()
                     .member(member)
                     .fcmToken(memberLoginReq.getFcmToken())
