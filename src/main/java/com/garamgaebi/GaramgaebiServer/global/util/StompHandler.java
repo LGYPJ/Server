dfdd572f10b88655792ea22ad3fbb9adb7e53ea9
Jodongjin <ehdwlsdlwkd@naver.com>
2023-03-31 00:47:07 +0900

    Refactor: 웹소켓 수정

--- src/main/java/com/garamgaebi/GaramgaebiServer/global/util/StompHandler.java
+++ src/main/java/com/garamgaebi/GaramgaebiServer/global/util/StompHandler.java
@@ -1,36 +1,50 @@
 package com.garamgaebi.GaramgaebiServer.global.util;
 
+import com.garamgaebi.GaramgaebiServer.domain.ice_breaking.dto.MemberRoomDeleteReq;
+import com.garamgaebi.GaramgaebiServer.domain.ice_breaking.service.GameService;
+import com.garamgaebi.GaramgaebiServer.domain.member.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.messaging.Message;
 import org.springframework.messaging.MessageChannel;
+import org.springframework.messaging.simp.SimpMessageSendingOperations;
 import org.springframework.messaging.simp.stomp.StompHeaderAccessor;
 import org.springframework.messaging.support.ChannelInterceptor;
 import org.springframework.stereotype.Component;
 import org.springframework.web.context.request.RequestContextHolder;
 
 @Component
+@RequiredArgsConstructor
 @Slf4j
 public class StompHandler implements ChannelInterceptor {
+
+    private final SimpMessageSendingOperations sendingOperations;
+    private final GameService gameService;
+
     @Override
     public void postSend(Message message, MessageChannel channel, boolean sent) {
         StompHeaderAccessor accessor = StompHeaderAccessor.wrap(message);
-        System.out.println("DEBUG, message: " + message);
-        System.out.println("DEBUG, accessor: " + accessor);
         String sessionId = accessor.getSessionId();
-        String userIdx = accessor.getFirstNativeHeader("userIdx");
-        log.info("DEBUG: userIdx = " + userIdx);
+
+        String memberIdx = accessor.getFirstNativeHeader("memberIdx");
+        String roomId = accessor.getFirstNativeHeader("roomId");
+        String nextMemberIdx = accessor.getFirstNativeHeader("nextMemberIdx");
+
+        log.info("DEBUG: memberIdx = {}", memberIdx);
+        log.info("DEBUG: roomId = {}", roomId);
+        log.info("DEBUG: nextMemberIdx = {}", nextMemberIdx);
 
         RequestContextHolder.getRequestAttributes();
 
         switch (accessor.getCommand()) {
             case CONNECT:
-                System.out.println("DEBUG, connected sessionId: " + sessionId + "userIdx: " + userIdx);
                 break;
             case DISCONNECT:
-                System.out.println("DEBUG, disconnected sessionId: " + sessionId + "userIdx: " + userIdx);
+                sendingOperations.convertAndSend("/topic/game/room/" + roomId, "DISCONNECT");
+                gameService.deleteMemberFromGameRoom(new MemberRoomDeleteReq(roomId, Long.parseLong(nextMemberIdx)), Long.parseLong(memberIdx));
                 break;
             case SUBSCRIBE:
-                System.out.println("DEBUG, subscribe sessionId: " + sessionId + "userIdx: " + userIdx);
                 break;
             default:
                 break;
